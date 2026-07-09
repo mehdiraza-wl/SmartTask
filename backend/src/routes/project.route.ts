0738e907de531707de47413a6f3fca71c6d3b72a
Mehdi Raza <mehdi.raza@wamolabs.com>
2026-07-09 20:54:45 +0500

    Refactor project routes for consistency and clarity

--- backend/src/routes/project.route.ts
+++ backend/src/routes/project.route.ts
@@ -1,25 +1,24 @@
 import  express  from "express";
 const router=express.Router();
 import passport from "../configs/passport.js"
-import { createProject, deleteProject, getAllProjects, getProject, sendExternalInvitation, sendProjectInvitation, sendExternalProjectView, getProjectMembers, updateProjectMember, removeProjectMember, updateProject, acceptInvite } from "../controllers/project.controller.js";
+import { createProject, deleteProject, getAllProjects, getProject, sendExternalInvitation, sendProjectInvitation, sendExternalProjectView, getProjectMembers, updateProjectMember, acceptInvite, updateProject } from "../controllers/project.controller.js";
 import { createProjectValidation } from "../middlewares/projectValidation.js";
 import validateRequest from "../middlewares/validateResult.js";
 import { authorizeProjectRoles } from "../middlewares/authorizeProjectRoles.js";
 
 
-router.use(router.use(passport.authenticate('jwt', { session: false })))
+router.use(passport.authenticate('jwt', { session: false }))
 
-router.get('/', getAllProjects)
-router.get('/:id', getProject)
-router.patch('/:id', authorizeProjectRoles(["admin"]), updateProject)
-router.post('/', createProjectValidation, validateRequest,createProject)
-router.delete('/:id', authorizeProjectRoles(["admin"]), deleteProject)
-router.get('/:id/members', getProjectMembers)
-router.patch('/:id/members/:userId', updateProjectMember)
-router.delete('/:id/members/:userId', removeProjectMember)
-router.post('/:id/invite', sendProjectInvitation)
-router.post('/invite/:token', acceptInvite)
+router.get('/', getAllProjects) 
+router.get('/:projectId', getProject)
+router.post('/', createProjectValidation, validateRequest,createProject) 
+router.patch('/:projectId', updateProject) 
+router.delete('/:projectId', authorizeProjectRoles(["admin"]), deleteProject) 
+router.get('/:projectId/members', getProjectMembers)
+router.patch('/:projectId/members/:userId', authorizeProjectRoles(["admin","manager"]), updateProjectMember) 
+router.post('/:projectId/invite', sendProjectInvitation) 
+router.post('/invite/:token', acceptInvite) 
 router.post('/:id/external-invite', sendExternalInvitation)
-router.get('/external-invite/:token', sendExternalProjectView)
+router.get('/external-invite/:token', sendExternalProjectView) 
 
 export default router;
