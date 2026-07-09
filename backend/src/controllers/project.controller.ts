543d2f7a93bd49774ae7b3357071733036aef650
Mehdi Raza <mehdi.raza@wamolabs.com>
2026-07-09 20:57:37 +0500

    Implement project management functionalities: enhance project retrieval, update member roles, and manage invitations

--- backend/src/controllers/project.controller.ts
+++ backend/src/controllers/project.controller.ts
@@ -6,96 +6,233 @@ import { ProjectCategory, ProjectInvitation, ProjectMember, ProjectTags, User } 
 import ProjectActivity from '../models/ProjectActivityLog.js';
 import crypto from 'node:crypto';
 import emailQueue from '../queues/email.queue.js';
-import { Op } from 'sequelize';
-import { use } from 'passport';
+import { Op, where } from 'sequelize';
+import projectTags from '../models/ProjectTags.js';
 
-export const getAllProjects = (req:Request, res:Response, next: NextFunction) => {
-    
+export const getAllProjects = async (req:Request, res:Response, next: NextFunction) => {
+    const userId = req.user!.id;
+
+    const memberships = await ProjectMember.findAll({
+        where: {
+            user_id: userId,
+            status: "active",
+        },
+        attributes: ["role"],
+        include: [
+            {
+                model: Project,
+                as: "project",
+                required: true,
+                include: [
+                    {
+                        model: ProjectCategory,
+                        as: "category",
+                        attributes: ["id", "category_name"],
+                    },
+                    {
+                        model: projectTags,
+                        as: "tags",
+                        attributes: ["id", "tag_name"],
+                    },
+                ],
+            },
+        ],
+    });
+
+    const projects = memberships
+        .filter((membership) => {
+            const project = membership.project!;
+
+            // Admins can see archived projects
+            if (membership.role === "admin") {
+                return true;
+            }
+
+            // Others cannot
+            return project.status !== "archived";
+        })
+        .map((membership) => ({
+            ...membership.project!.toJSON(),
+            role: membership.role,
+        }));
+
+    res.status(200).json({
+        projects,
+    });
+};
+
+export const getProject = async (req:Request, res:Response, next: NextFunction) => {
 }
 
-export const getProject = (req:Request, res:Response, next: NextFunction) => {
+export const getProjectMembers = async (req:Request, res:Response, next: NextFunction) => {
+    const { projectId } = req.params;
+    const members = await ProjectMember.findAll({
+        where: {
+            project_id: projectId,
+        },
+        attributes: ["status", "role", "joined_at"],
+        include: [
+            {
+                model: User,
+                as: "user",
+                attributes: ["id", "username", "email"],
+            },
+        ],
+        order: [
+            ["role", "ASC"],
+            ["joined_at", "ASC"],
+        ],
+    });
+    res.status(200).json({
+        members,
+    });
 }
 
-export const getProjectMembers = (req:Request, res:Response, next: NextFunction) => {
+export const updateProject = async (req:Request, res:Response, next: NextFunction) => {
+    const {projectId} = req.params
+    const {status} = req.body
+    await sequelize.transaction(async (transaction) => {
+        await Project.update(
+            { status },
+            {
+                where: {
+                    id: projectId,
+                },
+                transaction,
+            }
+        );
+
+        await ProjectActivity.create(
+            {
+                project_id: Number(projectId),
+                user_id: Number(req.user!.id),
+                action: "PROJECT_UPDATED",
+                description: `Project status updated to "${status}".`,
+            },
+            { transaction }
+        );
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "Project updated successfully.",
+    });
 }
-export const updateProjectMember = (req:Request, res:Response, next: NextFunction) => {
+
+export const updateProjectMember = async (req:Request, res:Response, next: NextFunction) => {
+    const { projectId, userId } = req.params;
+    const { role, status } = req.body;
+
+    const member = await ProjectMember.findOne({
+        where: {
+            project_id: projectId,
+            user_id: userId,
+        },
+    });
+
+    if (!member) {
+        throw new AppError("Project member not found.", 404);
+    }
+
+    await sequelize.transaction(async (transaction) => {
+    await member.update(
+        {
+            role,
+            status,
+        },
+        { transaction }
+    );
+
+    await ProjectActivity.create(
+        {
+            project_id: Number(projectId),
+            user_id: Number(req.user!.id),
+            action: "MEMBER_UPDATED",
+            description: `Updated member ${userId} with role '${role}' and status '${status}'`,
+        },
+        { transaction }
+    );
+});
+
+    res.status(200).json({
+        message: "Project member updated successfully.",
+        member,
+    });
 }
 
 export const createProject = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const {
-            title,
-            description,
-            category_id,
-            status,
-            owner_id,
-            tags
-        } = req.body;
+    console.log("Inside create");
+    
+    const {
+        title,
+        description,
+        category_id,
+        status,
+        tags
+    } = req.body;
 
-        const project = await sequelize.transaction(async (transaction) => {
+    const project = await sequelize.transaction(async (transaction) => {
 
-            const category = await ProjectCategory.findByPk(category_id, {
-                transaction,
-            });
-
-            if (!category) {
-                throw new AppError("Project category not found.", 500);
-            }
-
-            const project = await Project.create(
-                {
-                    owner_id,
-                    title,
-                    description,
-                    category_id,
-                    status,
-                    start_date: new Date(),
-                },
-                { transaction }
-            );
-
-            await ProjectMember.create(
-                {
-                    project_id: project.id,
-                    user_id: owner_id,
-                    role: "admin",
-                    status: "active",
-                    joined_at: new Date(),
-                },
-                { transaction }
-            );
-
-            await ProjectActivity.create(
-                {
-                    project_id: project.id,
-                    user_id: owner_id,
-                    action: "PROJECT_CREATED",
-                    description: `Created project "${project.title}"`,
-                },
-                { transaction }
-            );
-
-            if (tags?.length) {
-            await ProjectTags.bulkCreate(
-                tags.map((tag: string) => ({
-                    project_id: project.id,
-                    tag_name: tag.trim(),
-                })),
-                { transaction }
-                );
-            }
-
-            return project;
+        const category = await ProjectCategory.findByPk(category_id, {
+            transaction,
         });
 
-        res.status(201).json({
-            message: "Project created successfully.",
-            project,
-        });
+        if (!category) {
+            throw new AppError("Project category not found.", 500);
+        }
 
-    } catch (error) {
-        next(error);
-    }
+        console.log("Created");
+
+        const project = await Project.create(
+            {
+                owner_id: Number(req.user!.id),
+                title,
+                description,
+                category_id,
+                status,
+                start_date: new Date(),
+            },
+            { transaction }
+        );
+        
+        await ProjectMember.create(
+            {
+                project_id: project.id,
+                user_id: Number(req.user!.id),
+                role: "admin",
+                status: "active",
+                joined_at: new Date(),
+            },
+            { transaction }
+        );
+
+        await ProjectActivity.create(
+            {
+                project_id: project.id,
+                user_id: Number(req.user!.id),
+                action: "PROJECT_CREATED",
+                description: `Created project "${project.title}"`,
+            },
+            { transaction }
+        );
+
+        if (tags?.length) {
+        await ProjectTags.bulkCreate(
+            tags.map((tag: string) => ({
+                project_id: project.id,
+                tag_name: tag.trim(),
+            })),
+            { transaction }
+            );
+        }
+
+        return project;
+    });
+
+    res.status(201).json({
+        message: "Project created successfully.",
+        project,
+    });
 };
 
 export const deleteProject = async (req: Request, res: Response, next: NextFunction) => {
@@ -107,22 +244,38 @@ export const deleteProject = async (req: Request, res: Response, next: NextFunct
             throw new AppError("Project not found.", 404);
         }
 
-        await project.update({
-            status: "archived",
-        });
+        await sequelize.transaction(async (transaction) => {
+        await project.update(
+            {
+                status: "archived",
+            },
+            { transaction }
+        );
 
-        res.status(200).json({
-            success: true,
-            message: "Project archived successfully.",
-        });
+        await ProjectActivity.create(
+            {
+                project_id: project.id,
+                user_id: Number(req.user!.id),
+                action: "PROJECT_ARCHIVED",
+                description: `Archived project "${project.title}".`,
+            },
+            { transaction }
+        );
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "Project archived successfully.",
+    });
 
 };
 
 export const sendProjectInvitation = async (req:Request, res:Response, next: NextFunction) => {
-    const {project_id, invitedEmail, role} = req.body
+    const {projectId}=req.params
+    const {invitedEmail, role} = req.body
     const existing = await ProjectInvitation.findOne({
         where: {
-            project_id,
+            project_id: projectId,
             invitedEmail,
             status: "pending",
             expiry: {
@@ -136,7 +289,7 @@ export const sendProjectInvitation = async (req:Request, res:Response, next: Nex
     const inviteToken=crypto.randomBytes(20).toString("hex")
     const expiry=new Date(Date.now() + (7 * 24 * 60 * 60 * 1000)) //7 days expiry
     const projectInvitation=await ProjectInvitation.create({
-        project_id,
+        project_id: Number(projectId),
         token: inviteToken,
         invitedEmail,
         role,
@@ -146,8 +299,11 @@ export const sendProjectInvitation = async (req:Request, res:Response, next: Nex
         email: invitedEmail,
         subject: "Verification Code",
         message: `You can join the project using link: ${process.env.CLIENT_URL}/project/invite/${inviteToken}`,
-});
-    
+    });
+    res.status(200).json({
+        success: true,
+        message: "invitation sent successfully"
+    })
 }
 
 export const acceptInvite = async (req:Request, res:Response, next: NextFunction) => {
@@ -227,18 +383,8 @@ export const acceptInvite = async (req:Request, res:Response, next: NextFunction
         });
 }
 
-export const sendExternalInvitation = (req:Request, res:Response, next: NextFunction) => {
+export const sendExternalInvitation = async (req:Request, res:Response, next: NextFunction) => {
 }
 
-export const sendExternalProjectView = (req:Request, res:Response, next: NextFunction) => {
+export const sendExternalProjectView = async (req:Request, res:Response, next: NextFunction) => {
 }
-
-export const updateProject = (req:Request, res:Response, next: NextFunction) => {
-}
-
-
-export const removeProjectMember = (req:Request, res:Response, next: NextFunction) => {
-}
-
-
-
