e5d027e2bc9b5789db1960970100c4d7c569a0a9
Mehdi Raza <mehdi.raza@wamolabs.com>
2026-07-10 16:41:57 +0500

    Refactor task dependency management: move functionality to new controller and route files

--- backend/src/routes/task.route.ts
+++ backend/src/routes/task.route.ts
@@ -1,16 +1,16 @@
 import  express  from "express";
-import { assignTask, assignTaskDependency, createTask, deleteTaskDependency, getTaskDependency, removeTaskAssignment, updateTask} from "../controllers/task.controller.js";
+import { assignTask, createTask, removeTaskAssignment, updateTask} from "../controllers/task.controller.js";
 const router=express.Router({ mergeParams: true });
 import commentRouter from './comment.route.js'
 import { authorizeProjectRoles } from "../middlewares/authorizeProjectRoles.js";
+import taskDependencyRouter from "./taskDependency.route.js"
 router.use('/:taskId/comments',commentRouter)
+router.use('/:taskId/dependency', taskDependencyRouter)
 
 router.post('/', createTask)
 router.patch('/:taskId', updateTask)
 router.post('/:taskId/assignment', authorizeProjectRoles(["admin", "manager"]), assignTask)
 router.delete('/:taskId/assignment/:userId', authorizeProjectRoles(["admin", "manager"]), removeTaskAssignment)
-router.post('/:taskId/dependency', assignTaskDependency)
-router.get('/:taskId/dependency', getTaskDependency)
-router.delete('/:taskId/dependency/:dependencyId', deleteTaskDependency)
+
 
 export default router
